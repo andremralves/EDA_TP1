dc518fc4531a10388e391c711c75bb95fdd7bfd8
André Alves <andremralves@gmail.com>
2021-08-24 12:10:48 -0300

    Add a new project to git

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int menuNum;
+
+    do {
+        scanf("%d", &menuNum);
+        switch (menuNum) {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    } while (menuNum != 0);
+
+    return 0;
+}
