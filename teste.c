d8224b7e0a56679955c1aeecf99768a290f7fc92
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-08-31 19:18:36 -0300

    Codigo teste com funções finais consertadas

--- teste.c
+++ teste.c
@@ -9,23 +9,22 @@ struct palavra{
 typedef struct palavra Palavra; 
 
 Palavra* CopiaDados(Palavra* origem, Palavra* destino, int PosicaoLivre, int LenOrigem){
-    int valor = LenOrigem + 1;
-    for(int i = 0; i < valor; i++){
+    int IndexOrig = 0;
+    for(int i = IndexOrig; i < LenOrigem + 1; i++){
         if(i == PosicaoLivre)
-            i++;
+            continue;
         else{
-            destino[i].string = (char*)malloc(sizeof(char)*strlen(origem[i].string));
-            strcpy(destino[i].string,origem[i].string);
-            //printf("%s\n",destino[i].string);
+            destino[i].string = (char*)calloc(strlen(origem[IndexOrig].string),sizeof(char));
+            strcpy(destino[i].string,origem[IndexOrig].string);
+            IndexOrig++;
         }
     }
     return  destino;
 }
 
 Palavra* AlocaEspaco(Palavra* origem,int MaxLenVetor, int PosicaoLivre){
-    Palavra* vet_retorno = (Palavra*)malloc(MaxLenVetor + 1);
+    Palavra* vet_retorno = (Palavra*)calloc(MaxLenVetor + 1,sizeof(Palavra));
     vet_retorno = CopiaDados(origem,vet_retorno,PosicaoLivre,MaxLenVetor);
-    //printf("%s",vet_retorno[0].string);
     free(origem);
     return vet_retorno; 
 }
@@ -35,20 +34,16 @@ int findPos(Palavra* palavras,int NumElementos, char* elemento){
     int i, inicio, meio, final;
     inicio = 0;
     final = NumElementos - 1;
-    
     if(strcmp(elemento,palavras[0].string) < 0){
-        printf("%s\n",palavras[0].string);
         return 0;
     }
     while(inicio <= final){
         meio = (inicio + final)/2;
         if(strcmp(elemento,palavras[meio].string) < 0){
-            //printf("palavra: %s elemento_pos: %s inicio: %i meio: %i final: %i\n",palavras[meio].string,elemento,inicio,meio,final);
             final = meio - 1;
         }
         else{
             if(strcmp(elemento,palavras[meio].string) > 0){
-                //printf("palavra: %s elemento_pos: %s inicio: %i meio: %i final: %i\n",palavras[meio].string,elemento,inicio,meio,final);
                 inicio = meio + 1;
             }
             else{
@@ -69,17 +64,16 @@ Palavra* InsereOrdenado(Palavra* vetor, char* palavra_procurada, int* MaxLenVeto
     int posicao_insercao;
     if(*MaxLenVetor == 0){
         vetor = (Palavra*)malloc(sizeof(Palavra)*1);
-        vetor[0].string = (char*)malloc(sizeof(char)*strlen(palavra_procurada));
+        vetor[0].string = (char*)calloc(strlen(palavra_procurada),sizeof(char));
         strcpy(vetor[0].string,palavra_procurada);
         (*MaxLenVetor)++;
-        //printf("%s\n",vetor[0].string);
         return  vetor;
     }
     else{
         posicao_insercao = findPos(vetor,*MaxLenVetor,palavra_procurada);
         if(posicao_insercao >= 0){
             vetor = AlocaEspaco(vetor,*MaxLenVetor, posicao_insercao);
-            vetor[posicao_insercao].string = (char*)malloc(sizeof(char)*strlen(palavra_procurada));
+            vetor[posicao_insercao].string = (char*)calloc(strlen(palavra_procurada),sizeof(char));
             strcpy(vetor[posicao_insercao].string,palavra_procurada);
             (*MaxLenVetor)++;
         }
@@ -92,19 +86,16 @@ int main(){
     char* palavra_insere2 = "abacate";
     char* palavra_insere3 = "cachorro";
     char* palavra_insere4 = "zzz";
-    int teste;
     int MaxLenVetor = 0;
     
     palavras = NULL;
 
-    palavras = InsereOrdenado(palavras,palavra_insere2,&MaxLenVetor); 
+    palavras = InsereOrdenado(palavras,palavra_insere1,&MaxLenVetor); 
     palavras = InsereOrdenado(palavras,palavra_insere3,&MaxLenVetor); 
-    //teste = findPos(palavras,MaxLenVetor,palavra_insere4);
-    //printf("%i",teste);
-    palavras = InsereOrdenado(palavras,palavra_insere4,&MaxLenVetor);
+    palavras = InsereOrdenado(palavras,palavra_insere2,&MaxLenVetor);
     //palavras = InsereOrdenado(palavras,palavra_insere2,&MaxLenVetor);
-    for(int i = 0; i < 3; i++){
-        printf("%s\n",palavras[i].string);
-    }
+    //for(int i = 0; i < 3; i++){
+    //    printf("%s\n",palavras[i].string);
+    //}
     return 0;
 }
